f8e374f56dd9707c9bef004f430547973904ea8a
arkaprrava-wisdmlabs <arkaprava.chakraborty@wisdmlabs.com>
2023-06-30 21:44:45 +0530

    added verified icon to bbpress forum comment authors

--- public/class-wdm-verified-badge-icon-public.php
+++ public/class-wdm-verified-badge-icon-public.php
@@ -12,28 +12,39 @@ if(!class_exists('WDM_Verified_Badge_Icon_Public')){
          *
          * @param [String] $plugin_dir_path
          */
-        protected $users;
         public function __construct($plugin_dir_path){
             $this->plugin_dir_path = $plugin_dir_path;
         }
+        /**
+         * enqueue styles to frontend
+         *
+         * @return void
+         */
         public function wdm_enqueue_styles() {
             wp_enqueue_style( 'font-css', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/fontawesome.min.css',array(), time(), 'all');
         }
-        public function wdm_add_verification_bagdge_to_comments_authors($display_name, $order_id) {
+        /**
+         * adds verification badge to the comment author in both frontend and admin area
+         *
+         * @param [String] $display_name
+         * @param [Integer] $comment_id
+         * @return void
+         */
+        public function wdm_add_verification_bagdge_to_comments_authors($display_name, $comment_id) {
             $query = new WP_Query(array(
                 'post_type' => 'wc_user_membership',
                 'post_status' => 'wcm-active',
                 'post_parent' => 62
             ));
-            $this->users = array();
+            $users = array();
             while($query->have_posts()){
                 $query->the_post();
                 $author = get_the_author();
-                array_push($this->users,$author);
+                array_push($users,$author);
             }
             
             wp_reset_query();
-            if(in_array($display_name,$this->users)){
+            if(in_array($display_name,$users)){
                 $result = sprintf('%s <i title="%s" class="fa-solid fa-user"></i>',
                     $display_name,
                     __('This is a Verified Author', 'wdm-gsm')
@@ -42,11 +53,56 @@ if(!class_exists('WDM_Verified_Badge_Icon_Public')){
             }
             return $display_name;
         }
+        /**
+         * overrides the woocommerce my account dashboard page with it's own template
+         *
+         * @param [String] $template
+         * @param [String] $template_name
+         * @param [String] $template_path
+         * @return void
+         */
         public function wdm_override_woocommerce_template( $template, $template_name, $template_path ) {
             if ( $template_name === 'myaccount/dashboard.php' ) {
                 $template = $this->plugin_dir_path . 'templates/dashboard.php';
             }
             return $template;
+        }
+        /**
+         * add verified icon to forum comment authors
+         *
+         * @param [Array] $author_links
+         * @param [Array] $r
+         * @param [Array] $args
+         * @return void
+         */
+        public function wdm_bb_comment_author_badge($author_links, $r, $args){
+            if(isset($author_links[1])){
+                $query = new WP_Query(array(
+                    'post_type' => 'wc_user_membership',
+                    'post_status' => 'wcm-active',
+                    'post_parent' => 62
+                ));
+                $users = array();
+                while($query->have_posts()){
+                    $query->the_post();
+                    $author = get_the_author();
+                    array_push($this->users,$author);
+                }
+                
+                wp_reset_query();
+                $start = strpos($author_links[1],'>')+1;
+                $end = strpos($author_links[1],'</',$start);
+                $length = $end - $start;
+                $name = trim(substr($author_links[1],$start, $length));
+                if(in_array($name, $users)){
+                    $new_name = sprintf('%s <i title="%s" class="fa-solid fa-user"></i>',
+                        $name,
+                        __('This is a Verified Author', 'wdm-gsm')
+                    );
+                    $author_links[1] = substr_replace($author_links[1],$new_name, $start, $length);
+                }
+            }
+            return $author_links;
         }
     }
 }
--- wdm-verified-badge-icon.php
+++ wdm-verified-badge-icon.php
@@ -13,21 +13,39 @@
  */
 if( !class_exists('WDM_Verified_Badge_Icon')){
     class WDM_Verified_Badge_Icon{
+        /**
+         * define class variables and initials
+         */
         public function __construct(){
             $this->define_admin_hooks();
             $this->define_public_hooks();
         }
+        /**
+         * defines admin hooks
+         *
+         * @return void
+         */
         public function define_admin_hooks(){
             require_once plugin_dir_path( __FILE__ ).'admin/class-wdm-verified-badge-icon-admin.php';
             $admin = new WDM_Verified_Badge_Icon_Admin(plugin_basename( __FILE__ ));
             add_action('admin_init',array($admin,'wdm_has_dependencies'), 10, 0);
         }
+        /**
+         * defines public hooks
+         *
+         * @return void
+         */
         public function define_public_hooks(){
             require_once plugin_dir_path( __FILE__ ).'public/class-wdm-verified-badge-icon-public.php';
             $public = new WDM_Verified_Badge_Icon_Public(plugin_dir_path( __FILE__ ));
             add_action('wp_enqueue_scripts',array($public,'wdm_enqueue_styles'), 10, 0);
             add_action('comment_author',array($public,'wdm_add_verification_bagdge_to_comments_authors'), 10, 2);
             add_action('woocommerce_locate_template',array($public,'wdm_override_woocommerce_template'), 10, 3);
+            $pluginList = get_option( 'active_plugins' );
+            $plugin = 'bbpress/bbpress.php'; 
+            if ( in_array( $plugin , $pluginList ) ) {
+                add_filter('bbp_get_reply_author_links',array( $public, 'wdm_bb_comment_author_badge'), 10, 3);
+            }
         }
     }
 }
